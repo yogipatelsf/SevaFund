92c7f79b0b7610470fde48fe9f8bc55bd933da5e
sandond77 <sandu@ucdavis.edu>
2018-04-05 23:40:50 -0700

    Added schematypes to charities.js file

--- server/models/Charity.js
+++ server/models/Charity.js
@@ -1,34 +1,33 @@
-/**
- *
- * Example User model for mongodb
- *
- */
-
-/*
- * 
- * import mongoose from 'mongoose';
- * const Schema = mongoose.Schema;
- *
- * let UserSchema = new Schema({
- *   email: {type: String, required: true, unique: true, dropDups: true},
- *   password: {type: String, required: true},
- *   created_at: {type: Date, default: Date.now},
- *   updated_at: {type: Date},
- *   deleted_at: {type: Date}
- * });
- * export default mongoose.model('User', UserSchema);
- */
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 
 const CharitySchema = new Schema({
-	name: {
+	Name: {
 		type: String,
 		required: true
+	},
+	Supplier:{
+		type: String,
+	},
+	PurchaseOrder:{
+		type: Number
+	},
+	EthereumAddress:{
+		type: Number //
+	},
+	DesiredItems:{
+		type: Array,
+		default: []
+	},
+	CurrentFunds:{
+		type: Number,
+	},
+	StripeInfo:{
+		type: Array,
+		default: []
 	}
-})
+});
 
-const Charity = mongoose.model("Charities", CharitySchema)
+const Charity = mongoose.model("Charities", CharitySchema);
 
 exports default Charity;
