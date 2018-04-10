56fb4845f19eef85142f920313c725f910ec236c
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-09 22:58:20 -0700

    grabs object login data

--- frontend/src/components/SignUpModal.js
+++ frontend/src/components/SignUpModal.js
@@ -7,10 +7,12 @@ import Dialog, {
     DialogContentText,
     DialogTitle,
 } from 'material-ui/Dialog';
+import Navbar from './Navbar';
 
 class SignUpModal extends React.Component {
     state = {
         open: false,
+        logins: {}
     };
 
     handleClickOpen = () => {
@@ -20,6 +22,22 @@ class SignUpModal extends React.Component {
     handleClose = () => {
         this.setState({ open: false });
     };
+
+    //to pass the Login data
+    createLogin(event){
+        event.preventDefault();
+        console.log("This is me logging in");
+        const login = {
+            email: this.email.value,
+            username: this.username.value,
+            password: this.password.value,
+            status: this.status.value,
+        }
+        console.log(login);
+        this.loginForm.reset();
+
+    }
+    
     render() {
         return (
             <div>
@@ -29,44 +47,23 @@ class SignUpModal extends React.Component {
                     onClose={this.handleClose}
                     aria-labelledby="form-dialog-title"
                 >
-                    <DialogTitle id="form-dialog-title">Log In</DialogTitle>
+                    <DialogTitle id="form-dialog-title">Welcome to SevaFund</DialogTitle>
                     <DialogContent>
-                        <TextField 
-                            autoFocus
-                            ref="email"
-                            margin="dense"
-                            id="login-email"
-                            label="email address"
-                            type="email"
-                        />
-                        <br/>
-                        <TextField 
-                            autoFocus
-                            ref="username"
-                            margin="dense"
-                            id="login-email"
-                            label="username"
-                            type="username"
-                        />
-                        <br/>
-                        <TextField
-                            autoFocus
-                            ref="login-password"
-                            margin="dense"
-                            label="password"
-                            hintText="Password Field"
-                            floatingLabelText="Password"
-                            type="password"
-                        />
+                        <form ref={(input) => this.loginForm = input} className="login-edit" onSubmit={(e) => this.createLogin(e)}>
+                            <input ref={(input) => this.email = input} type="text" placeholder="email"/>
+                            <br/><br/>
+                            <input ref={(input) => this.username = input} type="text" placeholder="username"/>
+                            <br/><br/>
+                            <select ref={(input) => this.status = input}>
+                                <option value="Login">Login</option>
+                                <option value="Sign-Up">Sign Up</option>
+                            </select>
+                            <br/><br/>
+                            <input ref={(input) => this.password = input} type="text" placeholder="password"/>
+                            <br/><br/>
+                            <button type="submit">Login</button>
+                        </form>
                     </DialogContent>
-                    <DialogActions>
-                        <Button onClick={this.handleClose} color="secondary">
-                            Cancel
-                        </Button>
-                        <Button onClick={this.handleClose} color="secondary">
-                            Login
-                        </Button>
-                    </DialogActions>
                 </Dialog>
             </div>
         )
