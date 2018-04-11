af2022a871433ed27e19a0aef3acdec307c52017
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-10 23:51:33 -0700

    adds BrowserRouter, Match, Miss from 'react-router'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Match, Miss} from 'react-router';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
