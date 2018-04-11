f05921a0e1b39928068f087e13ef0c7a3b56730b
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-11 00:33:35 -0700

    targets App, NotFound are met on /query

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -3,7 +3,12 @@ import React from 'react';
 class NotFound extends React.Component {
     render() {
         return (
-            <h2>Not Found!</h2>
+            <div className="text-center">
+                <br/>
+                <h2>Not Found!</h2>
+                <p>We're very sorry, but the page you are looking for does not exist.</p>
+            </div>
+            
         )
     }
 }
