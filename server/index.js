6e1e684f59c9ee4f3ad6ffabc89fb78d56294ad6
A.Sofiane Guerfi <asofianeguerfi@calvisitor-10-105-162-127.calvisitor.1918.berkeley.edu>
2018-04-12 21:47:53 -0700

    db_fix

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const app = express();
 const SERVER_CONFIGS = require('./constants/server');
 
 const configureServer = require('./server');
 const configureRoutes = require('./routes');
+const mongoose = require('mongoose');
 
-const app = express();
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/SevaFund";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, () =>{
+  console.log("mongoose status code representations: 0 = disconnected/1 = connected/ 2 = connecting/ 3 = disconnecting. ")
+  console.log("mongoose connection status: Current status is: ", mongoose.connection.readyState);
+  
+});
+
 
 configureServer(app);
 configureRoutes(app);
