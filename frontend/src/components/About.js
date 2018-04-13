881cd7d0f4b5fe0328d79bea975e345630deaba5
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-13 13:10:57 -0700

    more content

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,20 +5,26 @@ const About = () => (
   <div className="container">
     
     <div className="content">
+      
       <div className="row">
         <h4 className="what-is-sevafund">What is SevaFund?</h4>
-        <p className="what-is-sevafund">
+        <p>
           Before we begin to answer that question, we would like to ask a few of our own. How do you serve your community? Do you volunteer? Are you a community organizer; someone who tries to bring people together in order to serve a philanthropic or meaningful purpose? From the simple act of giving to a loved one, to clothing, feeding, and providing medical services to the poor, we have all tried to make the world a better place. If you're coming to this site, you already practice "Seva," a Sanskrit word that embodies the act of selfless service or sacrifice without the want or need of reward or repayment.
         </p>  
         <p>
           Seva happens all over the world, everyday. In Sikh gurudwaras, Buddhist and Hindu temples, Islamic mosques, Jewish synagogues and Christian churches, Seva is prevalent in everyone's lives. We at SevaFund are just that: a diverse group of JavaScript Web Developers with various religious and cultural backgrounds just trying to make an impact in the world with the use of technology.
-        </p> 
+        </p>
+      </div>
+
+      <div className="row">
+        <h4 className="meet-the-team">Meet The Team</h4>
         <p>
           Here we are going to talk about what the foundation is and what we are
           envisioning for the future, the impact we want to have in the city of San
-          Francisco and eventually in the rest of the country and the world.
-        </p> 
-      </div>
+          Francisco and eventually in the rest of the country and the world.        
+        </p>
+      </div> 
+  
     </div>    
       
   </div>
