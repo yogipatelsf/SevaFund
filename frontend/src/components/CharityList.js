f838f32787d8f17d4dd80b168a6c63cc1bb97505
sandond77 <sandu@ucdavis.edu>
2018-04-16 00:11:45 -0700

    renamed gerardo's test charities from 'img' to 'image' to match with schema field for testing purposes

--- frontend/src/components/CharityList.js
+++ frontend/src/components/CharityList.js
@@ -9,7 +9,7 @@ class CharList extends Component {
   //   charities: [
   //     {
   //       title: "SF Marine Food Bank",
-  //       img: "https://farm3.staticflickr.com/2789/4115935787_9065bde73d.jpg",
+  //       image: "https://farm3.staticflickr.com/2789/4115935787_9065bde73d.jpg",
   //       content:
   //         "Our mission is to end hunger in San Francisco and Marin. We envision a community where everyone is able to obtain enough nutritious food to support the health and well-being of themselves and their families.",
   //       website: "https://www.sfmfoodbank.org/",
@@ -17,7 +17,7 @@ class CharList extends Component {
   //     },
   //     {
   //       title: "Project Open Hand",
-  //       img: "https://farm3.staticflickr.com/2391/2535419178_17da93473f.jpg",
+  //       image: "https://farm3.staticflickr.com/2391/2535419178_17da93473f.jpg",
   //       content:
   //         "Project Open Hand’s mission is to nourish and engage our community by providing meals with love to the sick and the elderly. Watch our video celebrating Project Open Hand clients Orazgul and Mario and driver, 'Jedi.'",
   //       website: "https://www.openhand.org/",
@@ -25,7 +25,7 @@ class CharList extends Component {
   //     },
   //     {
   //       title: "Covenant Food Pantry",
-  //       img: "https://farm6.staticflickr.com/5554/15071821931_9106dfb10b.jpg",
+  //       image: "https://farm6.staticflickr.com/5554/15071821931_9106dfb10b.jpg",
   //       content:
   //         "With God’s grace, we seek to share Christ through a welcoming and nurturing community of faith, doing God’s work in the world.",
   //       website: "http://www.covenantpcsf.org/Ministries/food_pantry.php",
