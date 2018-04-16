36d2aacd5a187d5a0e8374ad66784705913d94ee
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-15 21:44:36 -0700

    grammatical syntax error fixes on About page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -14,23 +14,20 @@ const Home = () => (
     </div>
 
     <div className="home">     
-        <div className="donor">
-            <div className="message">
-                <h4>Donor</h4>
-                <DonorModal/>
-                <p>Donate to the charities and projects of your choice and witness the difference you can make in an easy, open, and transparent way. Sign up here!</p>
-            </div>
-        </div>
+      <div className="donor">
+          <div className="message">
+              <h4>Donor</h4>
+              <DonorModal/>
+              <p>Donate to the charities and projects of your choice and witness the difference you can make in an easy, open, and transparent way. Sign up here!</p>
+          </div>
       </div>
 
-
-        <div className="charity">
-            <div className="message">
-                <h4>Charity</h4>
-                <CharityModal />
-                <p>Would you like your company to join the esteemed list of nonprofits that are experiencing the untapped well of investors and donors who want to see a new age of transparency? Sign up here!</p>
-            </div>
-        </div>
+      <div className="charity">
+          <div className="message">
+              <h4>Charity</h4>
+              <CharityModal />
+              <p>Would you like your company to join the esteemed list of nonprofits that are experiencing the untapped well of investors and donors who want to see a new age of transparency? Sign up here!</p>
+          </div>
       </div>
     </div>
   </div>
