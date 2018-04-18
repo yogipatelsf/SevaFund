cb04d1ae9e78bc23cc665a1939bb7b8db6435586
Ghataorhe <cssb-ghataorhe@calvisitor-10-105-170-104.calvisitor.1918.berkeley.edu>
2018-04-17 20:31:07 -0700

    adds more css changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,16 +4,25 @@ import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
+
+    const style = {
+      color: 'black',
+      position: 'fixed'
+    }
+
     return (
       <div>
-        <nav>
-          <div className="nav-wrapper light-blue accent-2">
-            <a href="/" className="brand-logo">
-              SevaFund
-            </a>
+        <nav className="transparent z-depth-0">
+          <div className="nav-wrapper" style={style}>
+
             <a href="" data-target="mobile-demo" className="sidenav-trigger">
               <i className="material-icons">menu</i>
             </a>
+
+            <a href="/" className="brand-logo">
+              SevaFund
+            </a>
+            
             <ul className="right hide-on-med-and-down">
               <a
                 className="waves-effect waves-light btn modal-trigger"
@@ -21,13 +30,22 @@ class Navbar extends Component {
               >
                 New Purchase Order
               </a>
-              <li>
-                <a href="/about">Who we are</a>
+              <li className="nav-words">
+                <a 
+                  href="/about"
+                >
+                Who we are
+                </a>
               </li>
-              <li>
-                <a href="/api/projects">Projects</a>
+              <li className="nav-words">
+                <a 
+                  href="/api/projects"
+                >
+                Projects
+                </a>
               </li>
             </ul>
+
           </div>
         </nav>
         <ul className="sidenav" id="mobile-demo">
