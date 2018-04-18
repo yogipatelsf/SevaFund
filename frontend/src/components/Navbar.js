11ed4d0cf4978d772e7a63f4154fab4902f3a352
Sandon Du <sandu@ucdavis.edu>
2018-04-17 21:47:18 -0700

    Revert "BGSevaFund11"

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,101 +3,45 @@ import SignUpModal from "./SignUpModal.js";
 import "./Navbar.css";
 
 class Navbar extends Component {
-  state = {
-    isTop: true,
-  };
-
-  componentDidMount() {
-    document.addEventListener('scroll', () => {
-      const isTop = window.scrollY < 100;
-      if (isTop !== this.state.isTop) {
-        this.setState({ isTop })
-      }
-    });
-  }
-  
   render() {
-
-    const overStyle = {
-      color: 'black',
-      position: 'fixed',
-      zIndex: '2',
-      margin: '0'
-    };
-
-    const navColor = {
-      backgroundColor: 'white'
-    }
-
-    const colorWords = {
-      color: 'black'
-    };
-
     return (
       <div>
-        <nav className="transparent z-depth-0">
-          <div className="nav-wrapper" style={overStyle}>
-            
-            <div className="row" style={navColor}>
-              <a href="" data-target="mobile-demo" className="sidenav-trigger">
-                <i className="material-icons" style={colorWords}>menu</i>
+        <nav>
+          <div className="nav-wrapper light-blue accent-2">
+            <a href="/" className="brand-logo">
+              SevaFund
+            </a>
+            <a href="" data-target="mobile-demo" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <a
+                className="waves-effect waves-light btn modal-trigger"
+                href="/newPo"
+              >
+                New Purchase Order
               </a>
-
-              <a href="/" className="brand-logo" style={colorWords}>
-                SevaFund
-              </a>
-              
-              <ul className="right hide-on-med-and-down">
-                <a
-                  className="waves-effect waves-light btn modal-trigger"
-                  href="/newPo"
-                >
-                  New Purchase Order
-                </a>
-                <li className="nav-words">
-                  <a 
-                    href="/about"
-                    style={colorWords}
-                  >
-                  Who we are
-                  </a>
-                </li>
-                <li className="nav-words">
-                  <a 
-                    href="/api/projects"
-                    style={colorWords}
-                  >
-                  Projects
-                  </a>
-                </li>
-              </ul>
-            </div>
-        
+              <li>
+                <a href="/about">Who we are</a>
+              </li>
+              <li>
+                <a href="/api/projects">Open PO's</a>
+              </li>
+            </ul>
           </div>
         </nav>
         <ul className="sidenav" id="mobile-demo">
           <a
             className="waves-effect waves-light btn modal-trigger"
-            style={colorWords}
             href="/newPo"
           >
             New Purchase Order
           </a>
           <li>
-            <a 
-              href="/about"
-              style={colorWords}
-            >
-              About
-            </a>
+            <a href="/about">About</a>
           </li>
           <li>
-            <a 
-              href="/api/projects"
-              style={colorWords}
-            >
-              Projects
-            </a>
+            <a href="/api/projects">Open PO's</a>
           </li>
         </ul>
       </div>
