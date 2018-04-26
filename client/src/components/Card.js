38ac910b19856746bc1739ac84611c1956357848
Mark Carlson <mark@markcarlson.io>
2018-04-25 21:47:06 -0700

    Compares todays time stamp with target date timestamp

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -31,22 +31,8 @@ class Card extends Component {
   render() {
     const { title, image, project, website, Amount, targetDate } = this.props;
     const { donation, funded} = this.state;
-    //code for creating todays date as a string instead of object
-    let date = new Date();
-    let dd = date.getDate();
-    let mm = date.getMonth()+1; //January is 0!
-    let yyyy = date.getFullYear();
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-
-    if(mm<10) {
-        mm = '0'+mm
-    } 
-    const today = mm + '/' + dd + '/' + yyyy; //to create todays date as a string
-    let todayDate = <Moment parse="MM/DD/YYYY" format="YYYY/MM/DD">{today}</Moment>;
-    let todayDateUnix = <Moment format="x">{today}</Moment>
-    let targetDateUnix = <Moment format="x">{targetDate}</Moment>
+    const todayDateUnix = Date.now();
+    const targetDateUnix = Date.parse(targetDate);
     let dates = [];
 
     return (
@@ -58,16 +44,12 @@ class Card extends Component {
           <div className="card-content">
             <span className="card-title activator grey-text text-darken-4 center-align">
               {title}
-              <p> {todayDateUnix} </p>
-              <p> {targetDateUnix} </p>
-              {console.log({todayDateUnix})}
-              {console.log({targetDateUnix})}
               <i className="material-icons right">more_vert</i>
             </span>
             <div className="container">
               {
-                (targetDateUnix > todayDateUnix) 
-                ? <div className="row">
+                (targetDateUnix > todayDateUnix)
+                ? <div className="row"> HERE
                   <form onSubmit={this.handleSubmit}>
                     <div className="col s6">
                       <label htmlFor="donate">Enter Amount</label>
@@ -112,7 +94,7 @@ class Card extends Component {
               -- Funded: {parseInt(funded, 10) / parseInt(Amount, 10) * 100}%
             </p>
             <p className="left-align">
-              Target Fulfillment: <Moment format="YYYY/MM/DD" add={{ hours: 7 }}>{targetDate}</Moment> 
+              Target Fulfillment: <Moment format="YYYY/MM/DD" add={{ hours: 7 }}>{targetDate}</Moment>
             </p>
             <p>
               <a href={website} target="_blank">
